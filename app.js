f326bcc991efcfd64438788ac5ca29a6731a010e
ShreyashB-11 <shreyash.bhovate@qsstechnosoft.com>
2025-08-07 21:34:46 +0530

    new Updated app.js

--- app.js
+++ app.js
@@ -2,13 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
-
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
 const html = `
 <!DOCTYPE html>
 <html>
@@ -59,3 +52,11 @@ const html = `
   </body>
 </html>
 `
+app.get("/", (req, res) => res.type('html').send(html));
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
+
